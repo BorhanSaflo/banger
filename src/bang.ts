a2bbf806b2b7f4a4181ab6d8a7be57e2b9d748cd
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2025-02-27 18:48:59 -0500

    inital commit

---
+++ src/bang.ts
@@ -0,0 +1,24 @@
+// This file was ripped from https://duckduckgo.com/bang.js
+import duckBangs from "./bangs.json" with {type: "json"};
+
+type Bang = {
+  c?: string;
+  d: string;
+  r: number;
+  s: string;
+  sc?: string;
+  t: string;
+  u: string;
+};
+
+if (typeof window !== "undefined") {
+    throw new Error("Attempted to re-map bangs on client side instead of at build time");
+}
+
+export const bangs: Record<string, Bang> = {
+  ...Object.fromEntries(
+    (duckBangs as Bang[]).map((bang) => {
+      return [bang.t, bang];
+    })
+  ),
+};
---
+++ src/main.ts
@@ -0,0 +1,76 @@
+import { bangs } from "./bang" with { type: "macro" };
+import "./global.css";
+
+function noSearchDefaultPageRender() {
+  const app = document.querySelector<HTMLDivElement>("#app")!;
+  app.innerHTML = `
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
+      <div class="content-container">
+        <h1>Unduck</h1>
+        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
+        <div class="url-container"> 
+          <input 
+            type="text" 
+            class="url-input"
+            value="${import.meta.env.BASE_URL}?q=%s"
+            readonly 
+          />
+          <button class="copy-button">
+            <img src="/clipboard.svg" alt="Copy" />
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
+  const copyIcon = copyButton.querySelector("img")!;
+  const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
+
+  copyButton.addEventListener("click", async () => {
+    await navigator.clipboard.writeText(urlInput.value);
+    copyIcon.src = "/clipboard-check.svg";
+
+    setTimeout(() => {
+      copyIcon.src = "/clipboard.svg";
+    }, 2000);
+  });
+}
+
+const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "ddg";
+
+function getBangredirectUrl() {
+  const url = new URL(window.location.href);
+  const query = url.searchParams.get("q")?.trim() ?? "";
+  if (!query) {
+    noSearchDefaultPageRender();
+    return null;
+  }
+
+  const match = query.match(/!(\S+)/i);
+
+  const bangCandidate = match?.[1]?.toLowerCase();
+  const selectedBang = bangs[bangCandidate ?? LS_DEFAULT_BANG];
+
+  // Remove the first bang from the query
+  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+
+  // Format of the url is:
+  // https://www.google.com/search?q={{{s}}}
+  const searchUrl = selectedBang?.u.replace(
+    "{{{s}}}",
+    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+    encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
+  );
+  if (!searchUrl) return null;
+
+  return searchUrl;
+}
+
+function doRedirect() {
+  const searchUrl = getBangredirectUrl();
+  if (!searchUrl) return;
+  window.location.replace(searchUrl);
+}
+
+doRedirect();
